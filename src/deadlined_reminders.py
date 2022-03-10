1e75793a6c0610830cb14d006debe6f4e3de669d
Anthony Dominguez <adominguez15722@gmail.com>
2022-03-09 18:11:14 -0600

    part 6 is complete

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -29,8 +29,8 @@ class DateReminder(DeadlinedReminder):
         self.date = parse(date, dayfirst = True)
 
 
-    def is_due(self, date):
-        self.date <= datetime.now()
+    def is_due(self):
+        return self.date <= datetime.now()
 
 
     
