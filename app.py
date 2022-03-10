df5d3326a9b55ea7d0cfd137b9ed0803f9067d50
Anthony Dominguez <adominguez15722@gmail.com>
2022-03-09 18:40:49 -0600

    part 8 is done

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ def handle_input():
     elif(choice == "2"):
         print()
         reminder = input("What would you like to be reminded about?: ")
+        date = input("When is that due?: ")
 
         add_reminder(reminder)
         list_reminders()
